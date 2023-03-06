f68e341623bb198034672379c3b450c2749b5c6a
Kyle Neal <nealkyle5@gmail.com>
2023-03-06 12:23:30 -0600

    dump wip: add some notes

---
+++ assets/static/docs/liveview_notes.md
@@ -0,0 +1,3 @@
+# My notes over learning LiveView
+- `live_patch` should be used when redirecting to same live view page, where as `live_redirect` should redirect to a different live view page
+- `PentoWeb.Router.Helpers.__info__(:functions)` shows functions of the module
--- lib/pento_web/endpoint.ex
+++ lib/pento_web/endpoint.ex
@@ -20,7 +20,7 @@ defmodule PentoWeb.Endpoint do
     at: "/",
     from: :pento,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: ~w(assets fonts images docs favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.

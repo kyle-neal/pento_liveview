4391bf429041242d3594c9d9472d62172d002c34
Kyle Neal <nealkyle5@gmail.com>
2023-03-06 10:32:32 -0600

    dump wip: fixed assets deploy to use static/ folder and removed priv/

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -1,4 +1,5 @@
 defmodule PentoWeb.WrongLive do
+  alias PentoWeb.WrongLive
   use Phoenix.LiveView, layout: {PentoWeb.LayoutView, "live.html"}
 
   def mount(_params, _session, socket) do
@@ -14,6 +15,9 @@ defmodule PentoWeb.WrongLive do
       <%= for n <- 1..10 do %>
         <a href="#" phx-click="guess" phx-value-number={n}><%= n %></a>
       <% end %>
+      <%= if @won_game do %>
+        <%= live_patch("Retry?", to: Routes.live_path(@socket, WrongLive)) %>
+      <% end %>
     </h2>
     <footer>
       <h4>It's now <%= @time %></h4>
@@ -22,16 +26,22 @@ defmodule PentoWeb.WrongLive do
   end
 
   def handle_event("guess", %{"number" => guess} = data, socket) do
-    {message, score} =
+    {message, score, guessed_correctly} =
       case guessed_correct?(guess) do
         true ->
-          {"You guessed correct!", socket.assigns.score + 1}
+          {"You guessed correct!", socket.assigns.score + 1, true}
 
         false ->
-          {"You guessed wrong :(", socket.assigns.score - 1}
+          {"You guessed wrong :(", socket.assigns.score - 1, socket.assigns.guessed_correctly}
       end
 
-    {:noreply, assign(socket, message: message, score: score, time: time())}
+    {:noreply,
+     assign(socket,
+       message: message,
+       score: score,
+       time: time(),
+       guessed_correctly: guessed_correctly
+     )}
   end
 
   defp guessed_correct?(guess) do
